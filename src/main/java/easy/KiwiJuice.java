56ef62bfc80cc77ef0f3c48b7e499756950fa27a
박유정 <dbwjd1154@gmail.com>
2023-07-25 17:26:05 +0900

    commit test

--- src/main/java/easy/KiwiJuice.java
+++ src/main/java/easy/KiwiJuice.java
@@ -6,11 +6,6 @@ class kiwiJuice {
 
     public static void main(String[] args) {
 
-//		int[] capacities = { 14, 35, 86, 58, 25, 62 };
-//		int[] bottles    = { 6, 34, 27, 38, 9, 60 };
-//		int[] fromId     = { 1, 2, 4, 5, 3, 3, 1, 0 };
-//		int[] toId       = { 0, 1, 2, 4, 2, 5, 3, 1 };
-
         int[] capacities = { 700000, 800000, 900000, 1000000 };
         int[] bottles    = { 478478, 478478, 478478, 478478 };
         int[] fromId     = { 2, 3 , 2, 0, 1};
